5abe89aca691d8ab636b03212ff8cd595634059e
Shreeshaa_F01 <shreeshapoojary100@gmail.com>
2025-10-24 11:20:08 +0000

    update 24/11

--- mix.c
+++ mix.c
@@ -15,6 +15,25 @@ void main()
         PA=(A*I*T)/100;
         printf("The the principle amount of the amount %lf duration %lf for intrest rate %lf is %lf rupees",A,T,I,PA);
         }
+        if(n==2)
+        {
+            int DD,MM,YYYY,AMT;
+            char reason[5];
+            printf("DAY BUDGETING \n MAJOR 5 TRANSACTION ");
+            printf("Enter the AMOUNT ");
+            scanf("%d",&AMT);
+            printf("ENter the DATE in DD MM YYY format");
+            scanf("%d %d %d",&DD,&MM,&YYYY);
+            printf("The cost Reason");
+            scanf("%s",&reason);
+            printf("THE AMOUNT %d was spent on %s on %d %d %d",AMT,reason,DD,MM,YYYY);
+
+        }
+        if(n!=1,2,3)
+        {
+            printf("the feature do not exist");
+        }
+
 }
 
         
